50fe4aa02f6d2cf0d81a429ddabde4dbc03e2937
Yingjie <52tiffany.yang@gmail.com>
2022-11-02 12:12:49 -0400

    First commit

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -1,35 +1,48 @@
 package com.zipcodewilmington.person;
 
 /**
- * Created by leon on 2/12/18.
+ * Created by Yingjie Yang on 12/02/2022
  */
 public class Person {
     private String name;
     private int age;
 
     public Person() {
+        name="";
+        age=Integer.MAX_VALUE;
     }
+    // can also be written as
+    // setName("");
+    // setAge(Integer.MAX_VALUE);
 
     public Person(int age) {
+        this.age =age;
     }
 
     public Person(String name) {
+        this.name =name;
     }
 
     public Person(String name, int age) {
+        this.name=name;
+        this.age = age;
+
     }
 
     public void setName(String name) {
+        this.name= name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+
+        return age;
     }
 }
